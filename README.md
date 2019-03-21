917fb906d66434a1ef6dd74cf387e88fdd01455c
yige <a@wyr.me>
2019-03-21 14:08:21 +0800

    修改打印程序

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-# node-pdf-printer
-Print PDF files in Windows OS (x64) by node.js.
+# Node.js PDF Printer
+Print PDF files in Windows OS by node.js.
 
 (Only node.js without c++).
 
-### reference
+## Usage
+```
+npm i node-pdf-printer
+```
 
-Thanks krdheeraj51.
+```javascript
+const NodePdfPrinter = require('node-pdf-printer')
+
+NodePdfPrinter.listPrinter() // List all printer name.
+
+const array = ['C:\\abc\\a.pdf', 'D:\\abd\\b.pdf']
+
+NodePdfPrinter.printFiles(array) // Printer files to the default Windows printer.
+
+NodePdfPrinter.printFiles(array, printerName) // Printer files to the specific printer by the printer name.
+```
+
+## Reference
+
+Thanks [krdheeraj51](https://github.com/krdheeraj51) and [qwqVictor](https://github.com/qwqVictor), [Columbia University](https://www.columbia.edu/).
 
 - https://github.com/krdheeraj51/node-printer
 - https://www.sumatrapdfreader.org/docs/Command-line-arguments.html
+- https://github.com/qwqVictor/PDFPrinter-WinPHP
+- http://www.columbia.edu/~em36/pdftoprinter.html
+
+## Legal Infomations
+- [PDFtoPrinter.exe](http://www.columbia.edu/~em36/pdftoprinter.html)'s all right reserved by Edward Mendelson in Columbia University.
--- src/index.js
+++ src/index.js
@@ -1,58 +1,54 @@
-const child_process = require('child_process')
+const childProcess = require('child_process')
 const iconv = require('iconv-lite')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
-   printFiles (pdfFiles, printerName) {
-      var createFile = "";
-      createFile += '@echo off \n';
-      createFile += 'cd \n';
+  printFiles (pdfFiles, printerName) {
+    return new Promise((resolve, reject) => {
+      let createFile = '@echo off \n'
+      createFile += 'cd \n'
 
-      if (printerName) {
-         for (var i = 0; i < pdfFiles.length; i++) {
-            createFile += 'SumatraPDF.exe -print-to "' + printerName +'" "' + pdfFiles[i] + '" \n';
-         }
-      } else {
-         for (var i = 0; i < pdfFiles.length; i++) {
-            createFile += 'SumatraPDF.exe -print-to-default "' + pdfFiles[i] + '" \n';
-         }
+      if (printerName) printerName = ' "' + printerName + '"'
+
+      for (var i = 0; i < pdfFiles.length; i++) {
+        createFile += 'PDFtoPrinter.exe "' + pdfFiles[i] + '"' + printerName + '\n'
       }
 
-      createFile += 'exit /b 0 \n';
+      createFile += 'exit /b 0 \n'
+      createFile += 'pause>nul \n'
 
-
-      createFile += 'pause>nul \n';
-
-      var fs = require("fs");
-      fs.writeFile("printpdffilesumatra.bat", createFile, function (err) {
-         if (err) {
-            console.log("something going wrong .....");
-         }
-      });
-
-      child_process.exec("printpdffilesumatra.bat", function (error, stdout) {
-         if (error) {
-            console.log("*************");
-            console.log(error);
-         } else {
-            console.log(stdout);
-         }
-      });
-   },
-   listPrinter () {
-      return new Promise((resolve, reject) => {
-         child_process.exec('%WINdir%\\System32\\cscript.exe %WINdir%\\System32\\printing_Admin_Scripts\\zh-CN\\prnmngr.vbs -l | find /i "打印机名"', {
-            encoding: 'buffer'
-         }, function (error, stdout) {
+      const batFileUrl = path.join(__dirname, 'printTmp.bat')
+      fs.writeFile(batFileUrl, createFile, function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          childProcess.exec(batFileUrl, function (error, stdout) {
             if (error) {
-               reject(error)
+              reject(error)
             } else {
-               stdout = iconv.decode(stdout, 'cp936').toString('utf-8').split('\r\n').map(item => {
-                  return item.substr(5)
-               })
-               stdout.pop()
-               resolve(stdout)
+              resolve(true)
             }
-         })
+          })
+        }
       })
-   }
+    })
+  },
+  listPrinter () {
+    return new Promise((resolve, reject) => {
+      childProcess.exec('%WINdir%\\System32\\cscript.exe %WINdir%\\System32\\printing_Admin_Scripts\\zh-CN\\prnmngr.vbs -l | find /i "打印机名"', {
+        encoding: 'buffer'
+      }, function (error, stdout) {
+        if (error) {
+          reject(error)
+        } else {
+          stdout = iconv.decode(stdout, 'cp936').toString('utf-8').split('\r\n').map(item => {
+            return item.substr(5)
+          })
+          stdout.pop()
+          resolve(stdout)
+        }
+      })
+    })
+  }
 }
